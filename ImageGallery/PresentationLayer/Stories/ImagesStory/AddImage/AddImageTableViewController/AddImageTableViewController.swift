5b197a5603b305535c477477edcdc04331fc7b55
Ihor Myroniuk <ihor.myroniuk@brander.ua>
2017-06-30 15:44:06 +0300

    Save.

--- ImageGallery/PresentationLayer/Stories/ImagesStory/AddImage/AddImageTableViewController/AddImageTableViewController.swift
+++ ImageGallery/PresentationLayer/Stories/ImagesStory/AddImage/AddImageTableViewController/AddImageTableViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class AddImageTableViewController: DefaultTableViewController, ImageAddImageTableViewCellControllerDelegate {
 
--- ImageGallery/PresentationLayer/Stories/ImagesStory/AddImage/AddImageViewController.swift
+++ ImageGallery/PresentationLayer/Stories/ImagesStory/AddImage/AddImageViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import SVProgressHUD
+import CoreLocation
 
-class AddImageViewController: UIViewController {
+class AddImageViewController: UIViewController, CLLocationManagerDelegate {
 
   let addImageTableViewController = AddImageTableViewController()
   private let customView = AddImageView()
@@ -22,6 +23,8 @@ class AddImageViewController: UIViewController {
     super.viewDidLoad()
 
     setupView()
+    setupLocationManager()
+    updateLocation()
   }
 
   func setupView() {
@@ -42,8 +45,19 @@ class AddImageViewController: UIViewController {
     let image = self.addImageTableViewController.imageAddImageTableViewCellController.image
     let description = self.addImageTableViewController.descriptionTableViewCellController.details
     let hashtag = self.addImageTableViewController.hashtagTableViewCellController.hashtag
+    if image == nil {
+      let alertController = UIAlertController(title: "Error", message: "Select image", preferredStyle: .alert)
+      let continueAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+      alertController.addAction(continueAction)
+      present(alertController, animated: true, completion: nil)
+    }
+    if currentLocation == nil {
+      updateLocation()
+    }
+    let latitude = Float((currentLocation?.coordinate.latitude)!)
+    let longitude = Float((currentLocation?.coordinate.longitude)!)
     SVProgressHUD.show()
-    APIImagesAddImage.requestWith(token: Application.user?.token, image: image!, description: description, hashtag: hashtag, latitude: 49.9935, longitude: 36.2304, success: { (image) in
+    APIImagesAddImage.requestWith(token: Application.user?.token, image: image!, description: description, hashtag: hashtag, latitude: latitude, longitude: longitude, success: { (image) in
       SVProgressHUD.dismiss()
       self.navigationController?.popViewController(animated: true)
     }) { (error) in
@@ -51,5 +65,56 @@ class AddImageViewController: UIViewController {
       APIErrorAlertController.show(error: error, in: self, animated: true)
     }
   }
+  
+  // MARK: Location
+  
+  let locationManager: CLLocationManager = CLLocationManager()
+  var currentLocation: CLLocation?
 
+  func setupLocationManager() {
+    locationManager.delegate = self;
+    locationManager.distanceFilter = kCLDistanceFilterNone;
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    locationManager.requestWhenInUseAuthorization();
+  }
+  
+  func updateLocation() {
+    if CLLocationManager.locationServicesEnabled() == false {
+      showLocationManagerAlertLocationServicesDisabled()
+    } else {
+      if CLLocationManager.authorizationStatus() == .denied {
+        showLocationManagerAlertAuthorizationStatusDenied()
+      } else {
+        locationManager.startUpdatingLocation();
+      }
+    }
+  }
+  
+  func showLocationManagerAlertLocationServicesDisabled() {
+    let alertController = UIAlertController(title: "Location Services Disabled", message: "Enable Location Services in Settings", preferredStyle: .alert)
+    let continueAction = UIAlertAction(title: "Continue", style: .cancel, handler: nil)
+    alertController.addAction(continueAction)
+    present(alertController, animated: true, completion: nil)
+  }
+  
+  func showLocationManagerAlertAuthorizationStatusDenied() {
+    let alertController = UIAlertController(title: "Authorization Status Denied", message: "Allow application use location services", preferredStyle: .alert)
+    let settingsAction = UIAlertAction(title: "Settings", style: .default) { (alertAction) in
+      guard let settingsUrl = URL(string: UIApplicationOpenSettingsURLString) else {
+        return
+      }
+      if UIApplication.shared.canOpenURL(settingsUrl) {
+        UIApplication.shared.open(settingsUrl, completionHandler: nil)
+      }
+    }
+    alertController.addAction(settingsAction)
+    let continueAction = UIAlertAction(title: "Continue", style: .cancel, handler: nil)
+    alertController.addAction(continueAction)
+    present(alertController, animated: true, completion: nil)
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    currentLocation = locations.first
+  }
+  
 }
